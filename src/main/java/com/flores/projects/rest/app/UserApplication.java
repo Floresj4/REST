56a3d743a6c9892e4dec1e4d89f0602ef840e017
Jason <floresj4@gmail.com>
2015-06-01 22:16:38 -0400

    adding @ApplicationPath, similar to url-pattern in deployment descriptor servlet-mapping

--- src/main/java/com/flores/projects/rest/app/UserApplication.java
+++ src/main/java/com/flores/projects/rest/app/UserApplication.java
@@ -1,5 +1,7 @@
 package com.flores.projects.rest.app;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.server.ResourceConfig;
 
 /**
@@ -8,6 +10,7 @@ import org.glassfish.jersey.server.ResourceConfig;
  * 
  * @author Jason
  */
+@ApplicationPath("resources")
 public class UserApplication extends ResourceConfig {
 	public UserApplication() {
 		packages("com.flores.projects.rest.service");
