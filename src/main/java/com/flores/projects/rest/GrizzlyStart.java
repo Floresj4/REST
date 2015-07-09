814a08bc2bf97bf05c343b66b3733d2d4982ab77
Jason <floresj4@gmail.com>
2015-07-08 22:34:43 -0400

    adding Grizzly client to run on heroku

---
+++ src/main/java/com/flores/projects/rest/GrizzlyStart.java
@@ -0,0 +1,25 @@
+package com.flores.projects.rest;
+
+import java.io.IOException;
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+
+import com.flores.projects.rest.app.UserApplication;
+
+/**
+ * 
+ * @author Jason
+ */
+public class GrizzlyStart {
+	public static void main(String args[]) throws IOException, InterruptedException {
+		URI baseUri = UriBuilder.fromUri("http://localhost").port(15351).build();
+
+		System.out.println("Starting Grizzly...");
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, new UserApplication());
+		server.start();	//how will heroku deal with shutdown?
+	}
+}
